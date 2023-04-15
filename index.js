b137055d2d40236f698dfeff2dc7093d74fa1ec5
tkanzariya <108585179+tkanzariya@users.noreply.github.com>
2023-04-15 23:39:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+var btnColours = ["red","yellow","blue","green"];
+var pattern = [];
+var userPattern = [];
+var level = 0;
+function playSound(picksound) {
+    var sounds = new Audio("sounds/"+picksound+".mp3")
+    return sounds.play();
+}
+
+function nextSequence(){
+    var randomTile = Math.floor(Math.random()*4);
+    var randColour = btnColours[randomTile];
+    pattern.push(randColour);
+    $("."+randColour).addClass("autopressed")
+    setTimeout(function(){
+        $("."+randColour).removeClass("autopressed")
+    },200); 
+    playSound(randColour);
+    $("h1").text("Level " + level);
+    level++;
+    
+}
+function wrongAnswer() {
+    var wrongsound = new Audio("sounds/wrong.mp3")
+    wrongsound.play();  
+    $("h1").text("Game Over, Press Any Key to Restart")
+    $("body").addClass("game-over")
+    setTimeout(function(){
+    $("body").removeClass("game-over")
+    },200)
+    level = 0;
+}
+
+function  repeatSqeuence(){
+    setTimeout(function(){
+        nextSequence();
+    },1000)
+}
+
+
+$(document).keypress(function(){  
+    pattern = [];
+    userPattern = [];
+    level = 0;
+    nextSequence();
+});
+function checkAnswer(){
+   
+var gameLength = userPattern.length - 1;
+
+if(pattern[gameLength] === userPattern[gameLength]){ 
+    if(pattern.length === userPattern.length){
+        setTimeout(function(){
+            nextSequence();
+        },1000)
+        userPattern=[];
+    }
+}
+else {
+    wrongAnswer();
+    Pattern = [];
+    userPattern = [];
+}
+}
+
+$(".btn").click(function(){
+    var mouseClick = $(this).attr("id");
+    userPattern.push(mouseClick);
+    $("."+mouseClick).addClass("pressed")
+    setTimeout(function(){
+        $("."+mouseClick).removeClass("pressed")
+    },100); 
+     playSound(mouseClick);
+     checkAnswer();
+
+}
+);
+
